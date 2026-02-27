d32f45cf9151a109a1c4d0122425b6d355c916b4
mhhong <mh.hong@proton.me>
2026-02-27 19:47:11 +0900

    Refactor sender and recipient variables

--- notification_manager.py
+++ notification_manager.py
@@ -11,10 +11,13 @@ class NotificationManager:
     def __init__(self):
         self.client = Client(self.twilio_account_sid, self.twilio_auth_token)
 
-    def send_whatsapp(self, message_body):
+    def send_whatsapp(self, message_body: str):
+        sender = os.getenv("TWILIO_WHATSAPP_SENDER_NUMBER")
+        recipient = os.getenv("TWILIO_WHATSAPP_RECIPIENT_NUMBER")
+
         message = self.client.messages.create(
-            from_ = f"whatsapp:{os.getenv("TWILIO_WHATSAPP_SENDER_NUMBER")}",
-            to = f"whatsapp:{os.getenv("TWILIO_WHATSAPP_RECIPIENT_NUMBER")}",
+            from_= f"whatsapp:{sender}",
+            to= f"whatsapp:{recipient}",
             body=message_body
         )
         print("MESSAGE SID: ", message.sid)
