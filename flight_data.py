ab7c961a6b6e8cf7533563ae4db3b110d52b2fec
mhhong <mh.hong@proton.me>
2026-02-27 19:08:39 +0900

    Refactor FlightData class with @dataclass decorator

--- flight_data.py
+++ flight_data.py
@@ -1,26 +1,36 @@
-class FlightData:
+from dataclasses import dataclass
 
-    def __init__(self, price, departure_airport, destination_airport, departure_date, return_date):
-        self.price = price
-        self.departure_airport = departure_airport
-        self.destination_airport = destination_airport
-        self.departure_date = departure_date
-        self.return_date = return_date
+@dataclass()
+class FlightData:
+    price: float
+    departure_airport: str
+    destination_airport: str
+    departure_date: str
+    return_date: str
+
 
 def find_cheapest_flight(data):
-    if data is None or not data["data"]:
+    if data  is None or not data ["data"]:
         print("No flight data")
-        return FlightData("N/A", "N/A","N/A","N/A","N/A")
+        return FlightData(0.00, "N/A","N/A","N/A","N/A")
+
+    print("AVAILABLE FLIGHT DATA: ", data)
 
     first_flight = data["data"][0]
     lowest_price = float(first_flight["price"]["grandTotal"])
-    departure = first_flight["itineraries"][0]["segments"][0]["departure"]["iataCode"]
-    destination = first_flight["itineraries"][0]["segments"][0]["arrival"]["iataCode"]
+    departure_code = first_flight["itineraries"][0]["segments"][0]["departure"]["iataCode"]
+    destination_code = first_flight["itineraries"][0]["segments"][0]["arrival"]["iataCode"]
     departure_date = first_flight["itineraries"][0]["segments"][0]["departure"]["at"].split("T")[0]
     return_date = first_flight["itineraries"][1]["segments"][0]["departure"]["at"].split("T")[0]
 
-    ## Initialize FlightData with the first flight for comparison
-    cheapest_flight = FlightData(lowest_price, departure, destination, departure_date, return_date)
+    ## Initialize FlightData with the first flight for comparison with the next flights of the rest
+    cheapest_flight = FlightData(
+        price= lowest_price,
+        departure_airport=departure_code,
+        destination_airport=destination_code,
+        departure_date=departure_date,
+        return_date=return_date
+    )
 
     for flight in data["data"]:
         price = float(flight["price"]["grandTotal"])
@@ -31,6 +41,7 @@ def find_cheapest_flight(data):
             departure_date = flight["itineraries"][0]["segments"][0]["departure"]["at"].split("T")[0]
             return_date = flight["itineraries"][1]["segments"][0]["departure"]["at"].split("T")[0]
             cheapest_flight = FlightData(lowest_price, departure, destination, departure_date, return_date)
+
             print(f"Lowest price to {destination} is ${lowest_price}")
 
     return cheapest_flight
